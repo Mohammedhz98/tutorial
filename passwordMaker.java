e276060bff94a25a4b68a34a97aa693d5ebe0e5e
Mohammedhz98 <mohammedhz98@hotmail.com>
2021-08-18 17:35:52 +0100

    A simple tool for making a minimal standard password

--- passwordMaker.java
+++ passwordMaker.java
@@ -1,7 +1,14 @@
 import java.util.*;
 public class passwordMaker {
+    public static ArrayList<String> passwords = new ArrayList<>();
 
-    // Scoring is rated from 1-10 (inclusive of 1 and 10).
+    /**
+     * This method is used to determine the score of a password inputted based on how strong it is.
+     * it takes 3 things into consideration which are: password length, password digits contained and
+     * password upper case letters contained.
+     * @param password The password that you would like to check the strength of.
+     * @return Returns a score of type "int"  on a scale form 1-10.
+     */
     public static int score(String password) {
         int strScore;
         if(password.length() >= 10 && upperCheck(password) && hasNumber(password)) {
@@ -21,33 +28,38 @@ public class passwordMaker {
         return strScore;
     }
 
+    /**
+     * A simple method which checks weather the inputted password contains an uppercase letter or not.
+     * @param pass The password that you would like to check to see if there are uppercase letters or not.
+     * @return Returns a boolean statement of true or false if the password has an uppercase letter or not.
+     */
     public static boolean upperCheck(String pass) {
-        boolean isUpper = false;
         for(int i=0; i<pass.length(); i++) {
             char passLetter = pass.charAt(i);
             if(Character.isUpperCase(passLetter)) {
-                isUpper = true;
-                return isUpper;
+                return true;
             }
         }
-        return isUpper;
+        return false;
     }
 
+    /**
+     * A simple method which checks weather the inputted password contains a number or not.
+     * @param pass The password that you would like to check to see if there are numbers or not.
+     * @return Returns a boolean statement of true or false if the password has a number or not.
+     */
     public static boolean hasNumber(String pass) {
-        boolean number = false;
         for(int i=0; i<pass.length(); i++) {
             char passLetter = pass.charAt(i);
             if(Character.isDigit(passLetter)) {
-                number = true;
-                return number;
+                return true;
             }
         }
-        return number;
+        return false;
     }
+
     public static void main(String[] args) {
-        //System.out.println(upperCheck("password"));
         boolean allPasswordsAdded = false;
-        ArrayList<String> passwords = new ArrayList<String>();
         while (!allPasswordsAdded) {
             System.out.println("Please input a new password: ");
             Scanner read = new Scanner(System.in);
@@ -55,29 +67,25 @@ public class passwordMaker {
             passwords.add(newPassword);
             System.out.println("Would you like to input another password? ");
             Scanner ask = new Scanner(System.in);
-            String response = read.nextLine();
+            String response = ask.nextLine();
             if (response.equals("no")) {
                 allPasswordsAdded = true;
             }
         }
-        System.out.println(passwords.size());
         for (int i=0; i<passwords.size(); i++) {
             boolean minimumStandardsMet = false;
             while (!minimumStandardsMet) {
                 if (passwords.get(i).length() < 10) {
-                    minimumStandardsMet = false;
                     System.out.println("The password: '" + passwords.get(i) + "' has a score of: " + score(passwords.get(i)));
                     System.out.println("The password you made was too short. it must have at least 10 letters please make a new one.");
                     Scanner retry = new Scanner(System.in);
                     passwords.set(i,retry.nextLine());
                 } else if (!upperCheck(passwords.get(i))) {
-                    minimumStandardsMet = false;
                     System.out.println("The password: '" + passwords.get(i) + "' has a score of: " + score(passwords.get(i)));
                     System.out.println("Your password has no uppercase numbers please input an uppercase letter into your password.");
                     Scanner retry = new Scanner(System.in);
                     passwords.set(i,retry.nextLine());
                 } else if (!hasNumber(passwords.get(i))) {
-                    minimumStandardsMet = false;
                     System.out.println("The password: '" + passwords.get(i) + "' has a score of: " + score(passwords.get(i)));
                     System.out.println("Your password has no numbers in it please input a number into your password.");
                     Scanner retry = new Scanner(System.in);
